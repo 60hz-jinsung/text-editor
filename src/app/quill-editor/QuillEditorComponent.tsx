86ea394c79db5ac7207a6c06738e7f6d71d67ecf
Jin-Sung Lee <102015109+60hz-jinsung@users.noreply.github.com>
2025-04-02 14:45:38 +0900

    fix: quill view 설정하였습니다

--- src/app/quill-editor/QuillEditorComponent.tsx
+++ src/app/quill-editor/QuillEditorComponent.tsx
@@ -6,16 +6,23 @@ import { useEffect, useRef, useState } from "react";
 
 export default function QuillEditorComponent() {
   const editorRef = useRef<HTMLDivElement>(null);
+  const viewerRef = useRef<HTMLDivElement>(null);
   const [htmlContent, setHtmlContent] = useState("");
 
   useEffect(() => {
-    if (!editorRef.current) return;
+    if (!editorRef.current || !viewerRef.current) return;
 
+    // 에디터 초기화
     const quill = new Quill(editorRef.current, {
       modules: {
         toolbar: [
           [{ header: [1, 2, false] }],
-          ["bold", "italic", "underline"],
+          ["bold", "italic", "underline", "strike"],
+          [{ color: [] }, { background: [] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ align: [] }],
+          [{ indent: "-1" }, { indent: "+1" }],
+          ["link"],
           ["image", "code-block"],
         ],
       },
@@ -23,24 +30,36 @@ export default function QuillEditorComponent() {
       theme: "snow",
     });
 
+    // 뷰어 초기화
+    const viewer = new Quill(viewerRef.current, {
+      modules: {
+        toolbar: false,
+      },
+      readOnly: true,
+      theme: "snow",
+    });
+
     quill.on("text-change", () => {
-      if (quill.getText().length <= 1) return setHtmlContent("");
-      setHtmlContent(quill.root.innerHTML);
+      if (quill.getText().length <= 1) {
+        setHtmlContent("");
+        viewer.setContents([{ insert: "\n" }]);
+      } else {
+        const content = quill.root.innerHTML;
+        setHtmlContent(content);
+        viewer.clipboard.dangerouslyPasteHTML(content);
+      }
     });
   }, []);
 
   return (
-    <div>
-      <div ref={editorRef}></div>
+    <div className="flex flex-col gap-y-[15px]">
       <div>
-        {htmlContent && (
-          <div className="mt-4 p-4 border rounded">
-            <h3 className="font-bold mb-2">HTML 내용:</h3>
-            <div className="p-2 bg-gray-100 rounded">
-              <pre className="whitespace-pre-wrap">{htmlContent}</pre>
-            </div>
-          </div>
-        )}
+        <h1 className="text-2xl font-bold mb-4">텍스트 에디터</h1>
+        <div ref={editorRef}></div>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold mb-4">텍스트 View</h1>
+        <div ref={viewerRef}></div>
       </div>
     </div>
   );
--- src/app/quill-editor/page.tsx
+++ src/app/quill-editor/page.tsx
@@ -3,7 +3,6 @@ import QuillEditorComponent from "./QuillEditorComponent";
 export default function QuillEditorPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">텍스트 에디터</h1>
       <QuillEditorComponent />
     </div>
   );
