b9da2450d03885f9a071e52a6ef658c2cf4392a8
Jin-Sung Lee <102015109+60hz-jinsung@users.noreply.github.com>
2025-04-02 13:32:41 +0900

    fix: jodit-editor view 기능 생성하였습니다

--- src/app/jodit-editor/JoditEditorComponent.tsx
+++ src/app/jodit-editor/JoditEditorComponent.tsx
@@ -7,7 +7,7 @@ const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 export default function JoditEditorComponent() {
   const [content, setContent] = useState("");
 
-  const config = useMemo(
+  const editConfig = useMemo(
     () => ({
       readonly: false,
       placeholder: "내용을 입력해주세요.",
@@ -20,16 +20,44 @@ export default function JoditEditorComponent() {
     []
   );
 
+  const viewConfig = useMemo(
+    () => ({
+      readonly: true,
+      toolbar: false,
+      showCharsCounter: false,
+      showWordsCounter: false,
+      language: "ko",
+      events: {
+        afterInit: (editor: any) => {
+          editor.container.addEventListener("click", (event: any) => {
+            const target = event.target as HTMLElement;
+            if (target.tagName === "A") {
+              const href = target.getAttribute("href");
+              if (href) {
+                window.open(href, "_blank");
+              }
+            }
+          });
+        },
+      },
+    }),
+    []
+  );
+
   return (
-    <div>
-      <JoditEditor
-        value={content}
-        config={config}
-        tabIndex={1}
-        onChange={(newContent) => {
-          setContent(newContent);
-        }}
-      />
+    <div className="flex flex-col gap-y-[15px]">
+      <div>
+        <h1 className="text-2xl font-bold mb-4">텍스트 에디터</h1>
+        <JoditEditor
+          value={content}
+          config={editConfig}
+          onBlur={(newContent) => setContent(newContent)}
+        />
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold mb-4">텍스트 View</h1>
+        <JoditEditor value={content} config={viewConfig} />
+      </div>
     </div>
   );
 }
--- src/app/jodit-editor/page.tsx
+++ src/app/jodit-editor/page.tsx
@@ -3,7 +3,6 @@ import JoditEditorComponent from "./JoditEditorComponent";
 export default function JoditEditorPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">텍스트 에디터</h1>
       <JoditEditorComponent />
     </div>
   );
